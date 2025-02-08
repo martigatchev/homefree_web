79a1d2fac5a13ac14ea3b93c699e073bd0145364
Marti Gatchev <mgatchev@gmail.com>
2025-02-08 14:12:53 -0800

    home page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Text, Flex, VStack } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/next-js";
 
 export default function Home() {
   return (
-    <Flex minH="100vh" align="center" justify="center">
-      <Box>
-        <Link href="/care_providers" color="blue.400" _hover={{ color: "blue.500" }}>
-        <Text color="blue.400" _hover={{ color: "blue.500", textDecoration: "underline" }}>
+    <Flex minH="100vh" align="center" justify="center" bg="gray.50" p={4}>
+      <VStack spacing={6}>
+        <Link href="/care_providers" _hover={{ textDecoration: "none" }}>
+          <Text 
+            color="blue.400" 
+            fontSize="2xl" 
+            fontWeight="semibold" 
+            _hover={{ color: "blue.500", textDecoration: "underline" }}
+          >
             Providers
           </Text>
         </Link>
-      </Box>
-      <Box>
-        <Text fontSize="4xl" fontWeight="bold">
+
+        <Text fontSize="4xl" fontWeight="bold" color="gray.700">
           HOME PAGE
         </Text>
-      </Box>
+      </VStack>
     </Flex>
   );
 }
+
